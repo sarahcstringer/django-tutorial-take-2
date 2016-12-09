ce0bfc951b8e8a44ab72ac61b6d803bd8017eda2
Sarah <sarahcstringer@gmail.com>
2016-12-09 12:19:57 -0800

    adding views for three first pages

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Question
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello, world. You\'re at the polls index.')
+    """Display 5 most recent questions"""
+
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+    """Display the details of an individual question"""
+
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    """Display the results of a question's votes"""
+
+    return render(request, 'polls/results.html')
+
+def vote(request, question_id):
+    """Vote for a question"""
+
+    return render(request, 'polls/vote.html')
